ad4e9db431e9e4f32a47b4e152a0f01ed8d4db79
MishaAkulenko <mishaakulenko@gmail.com>
2016-07-30 20:40:42 +0300

    First commit

---
+++ Task1/script.js
@@ -0,0 +1,10 @@
+/*document.write("Моя первая страница");
+function showMessage(){
+  alert ("Вы щелкнули по div-у");
+}*/
+function showAlert(){
+   var alerts=["one",'two','tree','for','five'];
+    for (var i = 0; i <= alerts.length-1; i++) {
+      alert (alerts[i]);
+     }
+}
---
+++ Task2/script.js
@@ -0,0 +1,10 @@
+
+/*document.getElementsByName('button').onClick = showAlert();
+function showAlert () {
+    var b=document.input.inp.value;
+alert (b);
+}*/
+function showAlert () {
+    var b=document.input.inp.value;
+alert (b);
+}
